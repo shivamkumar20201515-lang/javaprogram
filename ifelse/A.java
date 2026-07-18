02401262337dd488aff7deb7137f3ab1272cb5fd
shivamkumar <shivamkumar20201515gmail.com>
2026-07-18 21:22:14 +0530

    day2

---
+++ ifelse/A.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class A{
+	public static void main(String arg[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a number");
+		int no=sc.nextInt();
+		if(no>=0){
+			System.out.println("+ve");
+		}
+	}
+}
