ab7d32331f20491c680869fa32050eb62d8944ac
shivamkumar <shivamkumar20201515gmail.com>
2026-07-18 21:20:37 +0530

    day2

---
+++ basic/J.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class J{
+	public static void main(String arg[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the name ");
+		String name=sc.nextLine();
+		System.out.println("enter roll no ");
+		double roll=sc.nextDouble();
+		System.out.println("enter class");
+		int clas =sc.nextInt();
+		System.out.println("my name is "+name+" roll no is "+roll+" class is"+clas+" ");
+	}}
