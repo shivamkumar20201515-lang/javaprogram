2f711b96a73ce7ae547629cb6b83f9eb43f70988
shivamkumar <shivamkumar20201515gmail.com>
2026-07-24 22:19:56 +0530

    day 8

---
+++ looping/G.java
@@ -0,0 +1,16 @@
+class G{
+	public static void main(String arg[]){
+		  int i;
+         i=1;
+         System.out.print("A");
+         while(i<4)
+         {
+            System.out.print("B");
+            i++;
+             System.out.print("C");
+             break;
+          }
+           System.out.print("D");
+            System.out.print(i);
+	}
+}
