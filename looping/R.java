780121c76341d4049436116882fa09a50af978e9
shivamkumar <shivamkumar20201515gmail.com>
2026-07-26 21:48:48 +0530

    day 10

---
+++ looping/R.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Q{
+	public static void main(String arg[]){
+		Scanner sc = new Scanner(System.in);
+		int i,j;
+for(i=1;i<=3;i++)
+{
+    for(j=1;j<=4;j++)
+      {
+        System.out.print(j+"\t");
+      }
+    System.out.println();
+}}}
