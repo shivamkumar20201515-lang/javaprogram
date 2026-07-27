2525cb111818add59829b5a04f8edd54dd18616b
shivamkumar <shivamkumar20201515gmail.com>
2026-07-27 20:56:10 +0530

    day11

---
+++ looping/S.java
@@ -0,0 +1,11 @@
+class S{
+	public static void main(String arg[]){
+		int i,j;
+		for(i=1;i<=3;i++){
+			for(j=1;j<=4;j++){
+				System.out.print("*"+ " ");
+			}
+			System.out.println();
+		}
+	}
+}
